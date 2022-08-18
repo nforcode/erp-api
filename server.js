13b0f3b4f8422f6cadd747c69e869c589dd47baf
nforcode <nforcode@gmail.com>
2022-08-18 16:34:40 +0800

    fix server issue2

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const middlewares = jsonServer.defaults();
 
 // @see: https://github.com/typicode/json-server/issues/401
 const db = router.db;
-const jwtSecretKey = "test_key";
-const PORT = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+const PORT = process.env.PORT || 3000;
+const jwtSecretKey = process.env.JWT_KEY || "test_key";
 
 const ERROR_CODE = {
   INVALID: 1,
